d08ce546d3a2c8f255406875c73f5b113cbf2d67
Justin Owen <justin@bilbo.(none)>
2014-08-22 10:42:49 -0500

    working version

---
+++ 8.py
@@ -0,0 +1,12 @@
+# I had to use python 2 for this
+# I kept getting str does not support buffer error
+
+>>> pw = 'BZh91AY&SY\x94$|\x0e\x00\x00\x00\x81\x00\x03$ \x00!\x9ah3M\x13<]\xc9\x14\xe1BBP\x91\xf08'
+>>> un = 'BZh91AY&SYA\xaf\x82\r\x00\x00\x01\x01\x80\x02\xc0\x02\x00 \x00!\x9ah3M\x07<]\xc9\x14\xe1BA\x06\xbe\x084'
+
+>>> import bz2
+>>> bz2.decompress(un)
+'huge'
+>>> bz2.decompress(pw)
+'file'
+
